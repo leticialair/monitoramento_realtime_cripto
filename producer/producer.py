f6f49da483903dc2f6e661dbd42a3a65a6bb14a5
Letícia <leticialair@hotmail.com>
2025-04-03 23:12:17 -0300

    feat: producer binance

--- producer/producer.py
+++ producer/producer.py
@@ -1,13 +1,41 @@
-from kafka import KafkaProducer
+import json
 import time
+from kafka import KafkaProducer
+from websocket import create_connection
+
+LIST_SYMBOLS = [
+    "BTCUSDT",  # Bitcoin
+    "ETHUSDT",  # Ethereum
+    "BNBUSDT",  # Binance Coin
+    "XRPUSDT",  # Ripple
+    "ADAUSDT",  # Cardano
+    "DOGEUSDT",  # Dogecoin
+    "SOLUSDT",  # Solana
+    "MATICUSDT",  # Polygon
+    "DOTUSDT",  # Polkadot
+    "LTCUSDT",  # Litecoin
+    "SHIBUSDT",  # Shiba Inu
+    "TRXUSDT",  # Tron
+    "AVAXUSDT",  # Avalanche
+    "LINKUSDT",  # Chainlink
+    "ATOMUSDT",  # Cosmos
+    "UNIUSDT",  # Uniswap
+    "XLMUSDT",  # Stellar
+    "FILUSDT",  # Filecoin
+    "ETCUSDT",  # Ethereum Classic
+    "XMRUSDT",  # Monero
+]
+
+
+def get_connection(cripto_symbol):
+    ws = create_connection(f"wss://stream.binance.com:9443/ws/{cripto_symbol}@ticker")
+    return ws
 
 
 def create_producer():
     for _ in range(5):
         try:
-            return KafkaProducer(
-                bootstrap_servers=["kafka:9092"], api_version=(2, 0, 2)
-            )
+            return KafkaProducer(bootstrap_servers="kafka:9092")
         except:
             print("Waiting for Kafka...")
             time.sleep(5)
@@ -16,9 +44,31 @@ def create_producer():
 
 producer = create_producer()
 
-for i in range(10):
-    producer.send("test-topic", value=f"Mensagem {i}".encode("utf-8"))
-    print(f"Sent message {i}")
-    time.sleep(1)
+# while True:
+#     symbol = "BTCUSDT"
+#     symbol = symbol.lower()
+#     ws = get_connection(symbol)
+#     result = ws.recv()
+#     message = result.encode("utf-8")
+#     producer.send("cripto-topic", message)
+#     producer.flush()
+#     print("Mensagem enviada ao tópico cripto-topic!", symbol, result)
+#     time.sleep(1)
 
-producer.flush()
+while True:
+    for symbol in LIST_SYMBOLS:
+        try:
+            ws = create_connection(
+                f"wss://stream.binance.com:9443/ws/{symbol.lower()}@trade"
+            )
+            result = ws.recv()
+
+            message = {"symbol": symbol, "data": json.loads(result)}
+
+            producer.send("cripto-topic", message)
+            print(f"Enviado: {symbol}")
+
+        except Exception as e:
+            print(f"Erro com {symbol}: {str(e)}")
+
+        time.sleep(5)  # Intervalo entre símbolos
